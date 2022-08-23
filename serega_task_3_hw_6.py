e638e112a2dc2cf84d8ace73922becb37ca13735
Serega95bpk <109393577+Serega95bpk@users.noreply.github.com>
2022-08-23 23:30:23 +0300

    Delete serega_task_3_hw_6.py

--- serega_task_3_hw_6.py
+++
@@ -1,8 +0,0 @@
-country_capital = {'Mongolia': 'Ulaanbaatar', 'Ecuador': 'Quito', 'New Zealand': 'Wellington',
-'Argentina': 'Buenos Aires'}
-
-print(sorted(country_capital.items()))  # Сортировка словаря по ключу и вывод всего словаря
-
-print(sorted(country_capital.keys()))   # Вывод только ключей
-
-print(sorted(country_capital.values())) # Вывод только значений
