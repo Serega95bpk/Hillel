6ec915c9e3bde5c3dd6ac69f0a488f30e4d17b02
Serega95bpk <109393577+Serega95bpk@users.noreply.github.com>
2022-08-23 23:32:33 +0300

    Delete serega_task_6_hw_6.py

--- serega_task_6_hw_6.py
+++
@@ -1,9 +0,0 @@
-shedule = {'monday': ['clean house', 'drive car', 'meet with freands'], 'thuesday': None, 'wednesday': ['manicure', 'hammam', 'beer']}
-
-shedule_list = []
-for v in shedule.values():
-    shedule_list.append(v)
-
-shedule_list = shedule_list[0] + shedule_list[2]
-
-print(len(shedule_list))
